878c476a1d2660b56c2563620f871045e75f585f
stlls496 <https://github.com/stlls496/UnityGame>
2022-03-30 19:18:52 +0500

    Изменил вывод в консоль

--- Assets/Scripts/LoginGame.cs
+++ Assets/Scripts/LoginGame.cs
@@ -7,7 +7,7 @@ public class LoginGame : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello");
+        Debug.Log("Hello World");
     }
 
     // Update is called once per frame
