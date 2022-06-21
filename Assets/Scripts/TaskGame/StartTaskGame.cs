d691a22a613669cbf9dd00d421dbaf4f5c92fe8c
GlarkDen <90215968+GlarkDen@users.noreply.github.com>
2022-06-21 22:53:27 +0500

    Доп файлы

---
+++ Assets/Scripts/TaskGame/StartTaskGame.cs
@@ -0,0 +1,136 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartTaskGame : MonoBehaviour
+{
+    public GameObject startObject;
+
+    private static GameObject changeGame;
+
+    public static int CurrentTaskNumber { get; set; }
+
+    public static Text ClockTimer { get; set; }
+
+    public Text timer;
+
+    public Image showSolutionButton;
+
+    public Sprite loadSavingMap;
+
+    public int startTaskNumber;
+
+    private bool isSolution = false;
+
+    private Sprite saveSolutionButton;
+    private int[,] saveCurrentMap;
+    private int[] saveCurrentBlocks;
+
+    public static Task GetCurrentTask()
+    {
+        return tasks[CurrentTaskNumber];
+    }
+
+    public static void ChangeTask(int change)
+    {
+        if (CurrentTaskNumber + change >= tasks.Count)
+            CurrentTaskNumber = CurrentTaskNumber + change - tasks.Count;
+        else if (CurrentTaskNumber + change < 0)
+            CurrentTaskNumber = tasks.Count + CurrentTaskNumber + change;
+        else
+            CurrentTaskNumber += change;
+
+        NextTask();
+    }
+
+    public static void SetTask(int number)
+    {
+        CurrentTaskNumber = number;
+
+        NextTask();
+    }
+
+    public static List<Task> tasks = Serialization.LoadBinaryFile<List<Task>>(ProjectPath.Tasks);
+
+    void Start()
+    {
+        ClockTimer = timer;
+
+        startTaskNumber--;
+        CurrentTaskNumber = startTaskNumber;
+
+        changeGame = startObject;
+
+        foreach (var task in tasks)
+        {
+            task.mehanicBlock.CreateSprite();
+
+            foreach (var block in task.blocks)
+                block.CreateSprite();
+        }
+
+        changeGame.GetComponent<StartGameMechanic>().GenerateMap(tasks[CurrentTaskNumber].mapSize, (Block)tasks[CurrentTaskNumber].mehanicBlock.Clone());
+        changeGame.GetComponent<StartGameMechanic>().SetBlockCount(tasks[CurrentTaskNumber].blocks.Clone(), tasks[CurrentTaskNumber].countBlocks.Clone());
+        changeGame.GetComponent<StartGameMechanic>().SetTaskText(tasks[CurrentTaskNumber].text);
+
+        Timer.timer = ClockTimer;
+        Timer.stopTimer = StopTimer;
+        Timer.Start(new Clock(minutes: 10));
+    }
+
+    public static void ResetTimer()
+    {
+        Timer.Stop(false);
+        Timer.timer = ClockTimer;
+        Timer.stopTimer = StopTimer;
+        Timer.Start(new Clock(minutes: 10));
+    }
+
+    private static void StopTimer()
+    {
+
+    }
+
+    public static void NextTask()
+    {
+        VariablesMechanic.ClearTruthTable();
+        changeGame.GetComponent<StartGameMechanic>().RemoveMap();
+        changeGame.GetComponent<StartGameMechanic>().RemoveBlockCount();
+
+        changeGame.GetComponent<StartGameMechanic>().GenerateMap(tasks[CurrentTaskNumber].mapSize, (Block)tasks[CurrentTaskNumber].mehanicBlock.Clone());
+        changeGame.GetComponent<StartGameMechanic>().SetBlockCount(tasks[CurrentTaskNumber].blocks.Clone(), tasks[CurrentTaskNumber].countBlocks.Clone());
+        changeGame.GetComponent<StartGameMechanic>().SetTaskText(tasks[CurrentTaskNumber].text);
+
+        ResetTimer();
+    }
+
+    public void ChangeGameAndSolution()
+    {
+        if (isSolution)
+        {
+            isSolution = false;
+            showSolutionButton.sprite = saveSolutionButton;
+            changeGame.GetComponent<StartGameMechanic>().ChangeMap(saveCurrentMap);
+            changeGame.GetComponent<StartGameMechanic>().SetBlocksCount(saveCurrentBlocks);
+            VariablesMechanic.CreateThuthTableWait();
+        }
+        else
+        {
+            saveCurrentBlocks = (int[])VariablesMechanic.CountSensorBlocks.Clone();
+            saveCurrentMap = (int[,])StartGameMechanic.mapTilesValue.Clone();
+            saveSolutionButton = showSolutionButton.sprite;
+            showSolutionButton.sprite = loadSavingMap;
+            isSolution = true;
+            changeGame.GetComponent<StartGameMechanic>().ChangeMap(tasks[CurrentTaskNumber].solution.mapTilesValue);
+            changeGame.GetComponent<StartGameMechanic>().ClearBlockCount();
+            VariablesMechanic.CreateThuthTableWait();
+        }
+    }
+
+    public void SolutionToGame()
+    {
+        if (isSolution)
+            ChangeGameAndSolution();
+    }
+}
